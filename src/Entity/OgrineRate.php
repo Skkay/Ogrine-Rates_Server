c338806f93aff5f83368fe92f190e81c3138267d
Skkay <skkay.thomas@gmail.com>
2022-10-22 12:14:37 +0200

    Add unique constraint to 'datetime, rate_tenth'

--- src/Entity/OgrineRate.php
+++ src/Entity/OgrineRate.php
@@ -7,6 +7,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: OgrineRateRepository::class)]
+#[ORM\UniqueConstraint(name: 'ogrine_rate_idx', columns: ['datetime', 'rate_tenth'])]
 class OgrineRate
 {
     #[ORM\Id]
